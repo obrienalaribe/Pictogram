3f19047aad5d0594ac4553efef2b1840bc5c07d9
oalaribe <obrien.alaribe@manticpoint.com>
2017-04-05 16:49:03 +0100

    Initial Commit

---
+++ Pictogram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pictogram
+//
+//  Created by Obrien Alaribe on 05/04/2017.
+//  Copyright © 2017 obrien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
